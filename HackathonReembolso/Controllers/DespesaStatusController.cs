13134395c098e46d6af78432691a16fadbb1fe29
Renato Matos <renato.matos79@gmail.com>
2017-11-25 04:43:37 -0800

    status da despesa

---
+++ HackathonReembolso/Controllers/DespesaStatusController.cs
@@ -0,0 +1,37 @@
+﻿using HackathonReembolso.Framework.Mvc.Model;
+using HackathonReembolso.Mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HackathonReembolso.Mvc.Controllers
+{
+    public class DespesaStatusController : Controller
+    {
+        [HttpGet]
+        public JsonResult GetAll()
+        {
+            var result = new JsonResponse();
+            try
+            {
+                var response = new List<DespesaStatus>
+                {
+                    new DespesaStatus { Id = 1, Sigla = "C", Descricao = "Cadastro" },
+                    new DespesaStatus { Id = 2, Sigla = "R", Descricao = "Rejeitada" },
+                    new DespesaStatus { Id = 3, Sigla = "A", Descricao = "Aprovada" },
+                    new DespesaStatus { Id = 4, Sigla = "K", Descricao = "Cancelada" }
+                };
+                result = new JsonResponse { Data = response };
+            }
+            catch (Exception ex)
+            {
+                result.Message = "Houve um erro ao processar sua requisição! Favor tente novamente!";
+                result.Errors.Add(ex.ToString());
+                result.Result = false;
+            }
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ HackathonReembolso/Models/DespesaStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace HackathonReembolso.Mvc.Models
+{
+    [Serializable]
+    public class DespesaStatus
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Descricao { get; set; }
+        [DataMember]
+        public string Sigla { get; set; }
+    }
+}
